d2ffddb27c978c46af9769ea5bfb64de568823e1
Leigh West <leighwest@hotmail.com>
2022-02-05 22:28:13 +1100

    Minor view amendment to checkbox. Added plural logic to todo count in footer

--- app/client/src/Components/Footer/Footer.js
+++ app/client/src/Components/Footer/Footer.js
@@ -11,9 +11,19 @@ const Footer = memo(() => {
 
   let ctx = useContext(TodoContext);
 
+  let tasksToDo = '';
+
+  if (ctx.todos.length === 1) {
+    tasksToDo = `${ctx.todos.length} TASK`;
+  }
+
+  if (ctx.todos.length > 1) {
+    tasksToDo = `${ctx.todos.length} TASKS`;
+  }
+
   return (
     <div className={classes.wrapper}>
-      <p className={classes.todosCount}>{ctx.todos.length} TASKS</p>
+      <p className={classes.todosCount}>{tasksToDo}</p>
       <Button type={"add-new-task-btn"}/>
     </div>
   );
