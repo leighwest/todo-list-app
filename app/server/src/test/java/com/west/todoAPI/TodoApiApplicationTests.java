cebe06d606c181aec15d08c0e7f130515bc14bf4
Leigh West <leighwest@hotmail.com>
2022-02-06 12:59:40 +1100

    Added unit test for findall method

--- app/server/src/test/java/com/west/todoAPI/TodoApiApplicationTests.java
+++ app/server/src/test/java/com/west/todoAPI/TodoApiApplicationTests.java
@@ -1,13 +1,48 @@
 package com.west.todoAPI;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.west.todoAPI.entities.Todo;
+import com.west.todoAPI.services.TodoServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-@SpringBootTest
-class TodoApiApplicationTests {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+
+public class TodoApiApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	private TodoServiceImpl todoService;
 
 	@Test
-	void contextLoads() {
+	public void testFindAll() throws Exception {
+
+		Todo todo = new Todo();
+		todo.setId(1);
+		todo.setDescription("Practice coding");
+		todo.setDate(LocalDate.now());
+		todo.setCompleted(false);
+
+		List<Todo> todos = Arrays.asList(todo);
+		when(todoService.getTodos()).thenReturn(todos);
+
+		mockMvc.perform(get("/todoapi/todos/").contextPath("/todoapi")).andExpect(status().isOk());
 	}
 
 }
