bb3e88215ba4b2ae8e8dfa10d9ab226c11ef7699
Leigh West <leighwest@hotmail.com>
2023-07-16 04:25:56 +0000

    Updated todo controller swagger documentation

--- app/server/src/main/java/com/west/todoAPI/controllers/TodoRestController.java
+++ app/server/src/main/java/com/west/todoAPI/controllers/TodoRestController.java
@@ -7,7 +7,9 @@ import com.west.todoAPI.services.TodoService;
 import com.west.todoAPI.validator.TodoValidator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +39,10 @@ public class TodoRestController {
 
     @GetMapping(path="/todos", produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary="Returns a list of todos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content =
+                    { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", description = "No todos found")})
     public ResponseEntity<List<TodoDto>> getTodos() throws InterruptedException {
         LOGGER.info("Finding all todos");
         TimeUnit.SECONDS.sleep(1);
@@ -53,6 +59,10 @@ public class TodoRestController {
 
     @PostMapping(path="/todos", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary="Creates an individual todo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Creates a todo", content =
+                    { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "Bad request")})
     public @ApiResponse(description="Todo object") ResponseEntity<Object> createTodo(@RequestBody InitialTodoRequestModel todoDetails) {
         Optional<String> validationMessage = tv.validate(todoDetails);
 
@@ -70,6 +80,11 @@ public class TodoRestController {
     @PutMapping(path="/todos/{uuid}", consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE})
     @Operation(summary="Updates an individual todo by UUID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Updates a todo", content =
+                    { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Resource not found")})
     public @ApiResponse(description="Todo object") ResponseEntity<TodoDto> updateTodo(@Parameter(description="ID of the todo") @PathVariable("uuid") UUID uuid, @RequestBody UpdateTodoRequestModel todo) {
         TodoDto updatedTodo = todoService.update(uuid, todo);
 
@@ -77,8 +92,13 @@ public class TodoRestController {
     }
 
     @DeleteMapping(path="/todos/{uuid}", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    @Operation(summary="Deletes an individual todo by ID")
-    public void deleteTodo(@Parameter(description="ID of the todo") @PathVariable("uuid") UUID uuid) {
+    @Operation(summary="Deletes an individual todo by UUID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deletes a todo", content =
+                    { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Resource not found")})
+    public void deleteTodo(@Parameter(description="UUID of the todo") @PathVariable("uuid") UUID uuid) {
         LOGGER.info("Deleting todo with uuid: " + uuid);
         todoService.deleteByUuid(uuid);
     }
